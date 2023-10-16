fec308bbbcfff098952d9f971729b930393b30be
Rodrigo <rodrigo.estef@einstein.br>
2023-10-16 11:23:56 -0300

    init project

---
+++ src/test/java/com/estef/tetris/AppTest.java
@@ -0,0 +1,20 @@
+package com.estef.tetris;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
